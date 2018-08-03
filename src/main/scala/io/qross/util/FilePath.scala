83e3ce293da0bb43be2535e065010d3f7204f143
garfi-wu <garfi-wu@outlook.com>
2018-08-03 08:35:50 +0800

    v0.5.7 code sync

--- src/main/scala/io/qross/util/FilePath.scala
+++ src/main/scala/io/qross/util/FilePath.scala
@@ -7,7 +7,7 @@ object FilePath {
     def locate(path: String): String = {
         var full = path.replace("\\", "/")
         if (!full.startsWith("/") && !full.contains(":/")) {
-            full = Global.DATA_HUB_DIR + full
+            full = Global.QROSS_WORK_DIR + full
         }
         val parent = new File(full).getParentFile
         if (!parent.exists()) {
